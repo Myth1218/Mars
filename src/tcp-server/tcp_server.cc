9e972415a0fd61cca30fc2b2d94d4b057a446442
Myth <y13774404951z@163.com>
2023-05-30 19:41:38 +0800

    5_30

---
+++ src/tcp-server/tcp_server.cc
@@ -0,0 +1,83 @@
+#include "tcp_server.h"
+
+#include <sys/socket.h>     //是socket() bind() listen() accept()  connect()函数的头文件
+#include <sys/types.h>      //是socket() bind() listen() accept()  connect()函数的头文件
+
+#include <unistd.h>         //是close()函数的头文件
+#include <stdio.h>          //fprintf()的头文件
+#include <stdlib.h>         //exit()的头文件
+
+#include <strings.h>        //bzero()的头文件
+#include <arpa/inet.h>      //inet_aton()的头文件
+
+#include <signal.h>         //与信号相关的处理函数
+
+/// @brief 1. 创建socket 2.初始化服务器的地址 3. 绑定端口 4.监听 5.进行相应的优化:忽略某些信号 6.设置重复监听
+/// @param ip 
+/// @param port 
+
+Mars::Tcp_server::Tcp_server(const char* ip, uint16_t port) {
+//5. 忽略一些信号：SIGPIPE
+    if (signal(SIGHUP, SIG_IGN) == SIG_ERR) {     //出错处理
+        fprintf(stderr,"signal iganore SIGHUP \n");
+    }
+
+    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {     //出错处理
+        fprintf(stderr,"signal iganore SIGPIPE \n");
+    }
+
+//1. 创建监听套接字
+
+    //监听套接字是属于服务器端的一个数据成员
+    ///成功则为肺腑描述符；出错则为-1
+    listen_sockfd_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (listen_sockfd_ == -1) {
+        fprintf(stderr, "tcp::server :socket()\n");
+        exit(1);
+    }
+
+
+//2. 初始化服务器的协议地址
+
+    // 协议地址是32位的IPv4地址与16位的TCP或者UDP端口号的组合
+    struct sockaddr_in server_addr;
+    /*
+        void explicit_bzero(void *s, size_t n);
+        The bzero() function erases the data in the n bytes of the memory starting at the location pointed to by s, by writing zeros (bytes containing '\0') to that
+    area.
+    */
+    ///将内存空间清零
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;       //协议族：TCP/IP协议族
+    inet_aton(ip, &server_addr.sin_addr);   //将点分十进制形式的ip转换为一个32为得网络字节序二进制。server_addr.sin_addr是32为
+    server_addr.sin_port = htons(port);     //erver_addr.sin_por是16位。htons 将主机字节序转换为16位网络字节序
+
+//7. 设置sock可以重复监听
+    int op = 1;
+    if (setsockopt(listen_sockfd_, SOL_SOCKET, SO_REUSEADDR, &op, sizeof(op)) < 0) {
+        fprintf(stderr, "set socket reuse failed\n");
+        exit(1);
+    }
+
+//3. 绑定端口bind
+    //bind函数将一个本地协议地址赋予一个套接字
+    if (bind(listen_sockfd_, (const struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        fprintf(stderr, "bind error\n");
+        exit(1);
+    }
+//4. 监听listen
+    //当socket()创建的套接字是主动套接字，他应该用与调用connect发起连接的，listen()将一个未连接的套接字转换为一个被动套接字，指示内核应该接受指向该套接字的链接请求。
+    //listen的第二个参数规定了内核为相应套接字排队的最大连接个数(全连接队列和半连接队列之和小于该数)    
+    if (listen(listen_sockfd_, 500)  == -1) {
+        fprintf(stderr, "listen error\n");
+        exit(1);
+    }
+}   
+
+Mars::Tcp_server::~Tcp_server() {
+
+}
+
+void Mars::Tcp_server::do_accept() {
+    
+}
---
+++ src/tcp-server/tcp_server.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+#include <netinet/in.h>
+
+/* tcp服务器对外封装的接口。当我创建并初始化一个服务器对象后，相当于运行了一个服务器了，不需要关注服务器具体的细节
+
+int main () {
+    tcp_server server("127.0.0.1",7777);
+    server.do_accept();
+}
+
+*/
+
+namespace Mars {
+
+class Tcp_server {
+    public:
+        //构造函数
+        Tcp_server(const char* ip, uint16_t port); //short就是16位
+
+        //析构函数，释放资源
+        ~Tcp_server();
+
+        //服务器的链接客户端服务
+        void do_accept();
+
+    private:
+        int listen_sockfd_;                 //监听套接字：监听套接字是属于服务器端的一个数据成员
+        struct sockaddr_in client_addr_;    //已连接的对端进程（客户端）的协议地址
+        socklen_t client_addr_len_;         //内核存放在在套接字地址结构类的确切字节数
+
+    };
+
+}
+
+
